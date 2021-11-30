368617c649bfa8dcf5a5f30a427b8a45d750dae7
Liam Conroy (Student) <N00181800@iadt.ie>
2021-11-30 17:26:17 +0000

    List sales page

--- supplies-manager/src/pages/SalesPage.js
+++ supplies-manager/src/pages/SalesPage.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { Table } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 const SalesPage = () => {
 
-    const [sales, setSales] = useState([]) 
+    const [sales, setSales] = useState(null) 
 
     useEffect(() => {
         axios.get("http://localhost:8000/sales")
@@ -17,12 +20,38 @@ const SalesPage = () => {
 
 
 if (!sales) return null
+    
     const salesList = sales.map(sale => {
+
         return(
-            <div key = {sale._id}>
-                <p>{sale.couponUsed}</p>
-                <p>{sale.saleDate}</p>
-            </div>
+            <>
+
+            {/* <div key = {sale._id}> */}
+                {/* <Table bordered> */}
+                    <tbody>
+                    <tr>
+                        <td><Link to = {`/sales/${sale._id}`}>{sale.customer.email}</Link></td>
+                        <td>{sale.saleDate}</td>
+                        <td></td>
+                        <td>{sale.storeLocation}</td>
+
+                        <td className = "removePadding">
+                        
+                        <Button variant="warning" type = "submit">Edit</Button>
+                        <Button variant="danger" type = "submit">Delete</Button>
+                        
+                        </td>
+
+                    </tr>
+                    </tbody>
+                {/* </Table>  */}
+
+                {/* <p>{sale.couponUsed}</p>
+                <p>{sale.saleDate}</p> */}
+            {/* </div> */}
+
+            </>
+
         )
     })
 
@@ -31,8 +60,23 @@ if (!sales) return null
     return(
         <div className = "container">
             <div className = "mt-3">
-                <h3>Sales</h3>
+            <h3>Sales</h3>
+        
+        <div className = "col-lg-10">
+        <Table bordered size="sm">
+            <thead>
+                <tr>
+                    <th>CustEmail</th>
+                    <th>SaleDate</th>
+                    <th>NumItems</th>
+                    <th>StoreLocation</th>
+                    <th width = "16%">Options</th>
+                </tr>
+            </thead>
+        
                 { salesList }
+        </Table>
+        </div>
             </div>
         </div>
     )
--- supplies-manager/src/pages/SingleSale.js
+++ supplies-manager/src/pages/SingleSale.js
@@ -1,8 +1,71 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
+// import { Button } from 'react-bootstrap'
+
 const SingleSale = () => {
+    
+    let { id } = useParams()
+    // const [item, setItem] = useState(null)
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/sales/${id}`)
+        .then(response=> {
+            console.log(response.data.sale)
+            // setItem = (response.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    })
+
+    // if(!item) return null
+
+    // const listItems = item.maps(item => {
+    //    return (
+    //     <div>
+    //     <p>{item.name}</p>
+    //     </div>           
+    //    )
+        
+    // })
+
+    
+
     return(
+        <div className = "container">
+        <div className = "d-flex justify-content-xl-center">        
+        <div className = "mt-5 cardStyleAlt">
         <div>
-           <h2>Single Sale</h2> 
+            <h3 className = "nomargin">Sales Id: {id}</h3>
+            <p>{sale.couponUsed}</p>
+                 
         </div>
+
+
+        <div className = "d-flex justify-content-center mt-5">
+            <div className = "">
+                <div className = "mt-2">
+                <h3 className = "nomargin">Items Purchased </h3>
+                </div>
+                <Table bordered>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                </Table>
+            </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    </div>
     )
 }
 
